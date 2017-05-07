cbd90e2b35ba3da2394fe666b78748c1fc506d06
tapin13 <tapin13@gmail.com>
2017-05-07 04:02:30 +0300

    tutorial10_open_window

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-##openGL 3.3 Examples - ANSI C, Linux (LinuxMint or Ubuntu) 
+## openGL 3.3 Examples - ANSI C, Linux (LinuxMint or Ubuntu) 
 
 Adapted to work on LinuxMint (Ubuntu), GLEW, GLFW. Used ANSI C.
 
---
+++ tutorial10_open_window/open_window.c
@@ -0,0 +1,54 @@
+/* 
+ * File:   open_window.c
+ * Author: tapin13
+ *
+ * Created on May 6, 2017, 1:27 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define true  1
+#define false 0
+
+int main(int argc, char** argv) {
+    if(!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW.");
+        return EXIT_FAILURE;
+    }
+    
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Don't use old openGL
+    
+    GLFWwindow *window;
+    window = glfwCreateWindow(640, 480, "Tutorial 01", NULL, NULL);
+    if(window == NULL) {
+        fprintf(stderr, "Failed to open GLFW windows. If you have an Intel GPU, they are not 3.3 compatible.");
+        return EXIT_FAILURE;
+    }
+    
+    glfwMakeContextCurrent(window);
+    glewExperimental = true;
+    if(glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to init GLEW");
+        return EXIT_FAILURE;
+    }
+    
+    // Ensure we can capture Esc.
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    
+    while(glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0) {
+        // draw here!
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    return (EXIT_SUCCESS);
+}
+
