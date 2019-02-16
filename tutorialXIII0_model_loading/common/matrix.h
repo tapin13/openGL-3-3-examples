ea8fb0973dad987d0026512ef1b2180ac0bce353
tapin13 <tapin13@gmail.com>
2019-02-16 03:17:21 +0200

    fix pi

--- tutorialXIII0_model_loading/common/matrix.h
+++ tutorialXIII0_model_loading/common/matrix.h
@@ -7,7 +7,9 @@
 
 #include <GL/glew.h>
 
-# define M_PI		3.14159265358979323846	/* pi */
+#ifndef M_PI
+#define M_PI    3.14159265358979323846	/* pi */
+#endif
 
 // angle convert degree to radian
 float deg2rad(float angle);
